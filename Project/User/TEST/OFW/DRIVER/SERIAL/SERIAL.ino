3d0369f899a9d83ae1e26327181fc0f587bc2397
int71 <i71rsk75856@gmail.com>
2024-03-23 17:00:54 +0900

    update for '2024/03/23' by int71

--- Project/User/TEST/OFW/DRIVER/SERIAL/SERIAL.ino
+++ Project/User/TEST/OFW/DRIVER/SERIAL/SERIAL.ino
@@ -1,8 +1,9 @@
+//	use "switch_i71.bat"
 #include				<ofw_driver_serial0.hpp>
 
 int main(void){
 	OFW::stNew();
-	driver::SERIAL0::stNew(230400);
+	driver::SERIAL0::stNew(115200);
 	driver::SERIAL0::stPrintClear();
 	for(;;){
 		driver::SERIAL0::stPrint(ARGSTR("TEST\r\n"));
